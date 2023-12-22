ca13f7592d7a4cdcf06b5c810cdd5735c5f1c403
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-22 12:18:47 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
+import HelloWorld from "./components/HelloWorld";
 
-export default function App() {
-  return <div>Replace me with your Component!</div>;
+export default function HelloWorldArticle() {
+  return <HelloWorld />;
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+  return (
+    <article>
+      <h1>Überschrift</h1>
+      <p>Hier soll noch ein Text stehen</p>
+    </article>
+  );
+}
